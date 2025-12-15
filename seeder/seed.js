5b26e57c7dbca8361623e87acbc8ca623c34476f
Thibaud <thibaudfresnay.pro@gmail.com>
2025-12-15 15:55:43 +0100

    I updated seed in order to add Jokes in sqlite file only if there were no other instancied Jokes

--- seeder/seed.js
+++ seeder/seed.js
@@ -4,10 +4,13 @@ import seeds from "./jokes.json" with {type: "json"}
 
 const seedDatabase = async () => {
     try {
-        const jokes = await Joke.bulkCreate(seeds)
-        console.log(`initial joke instancied`)
-        return jokes
-    } catch(error){
+        const jokesCount = await Joke.count();
+        if (jokesCount === 0) {
+            const jokes = await Joke.bulkCreate(seeds)
+            console.log(`initial joke instancied`)
+            return jokes
+        }
+    } catch (error) {
         console.log(`error while seeding: ${error}`)
         throw error;
     }
